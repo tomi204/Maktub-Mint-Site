aabc72b542223ab7cf48a09313410cf7cc2418b7
tomi204 <tomi204@outlook.com>
2023-02-18 22:35:49 -0300

    update styles and conditional

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,6 +9,8 @@ import {
   dropTotalClaimedSupplyQuery,
 } from "@thirdweb-dev/react/solana";
 import { MediaRenderer } from "@thirdweb-dev/react";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { Wallet } from "@project-serum/anchor";
 
 // Default styles that can be overridden by your app
 require("@solana/wallet-adapter-react-ui/styles.css");
@@ -19,6 +21,7 @@ const WalletMultiButtonDynamic = dynamic(
   { ssr: false }
 );
 const Home = () => {
+  const wallet = useWallet();
   const { program } = useProgram(
     "4Rhgk4qy53uTavYFMupgHUfsRbm5gGVpcqJBvwn7cCEZ",
     "nft-drop"
@@ -37,45 +40,48 @@ const Home = () => {
   const nft = metadata?.filter(
     (nft) => nft.metadata.name === "NFT Asset Name #7"
   );
-  console.log(nft, "nfttt");
+
   return (
     <>
       <div className={styles.container}>
         <navbar className={styles.navbar}>
           <WalletMultiButtonDynamic />
         </navbar>
-        <div className={styles.iconContainer}>
-          <Image
-            width={75}
-            height={75}
-            src="/sol.png"
-            className={styles.icon}
-            alt="sol"
-          />
-        </div>
 
         <div className={styles.nftContainer}>
-          {nft?.map((nft) => (
-            <div key={nft.metadata.id} className={styles.nftCard}>
-              <MediaRenderer
-                src={nft.metadata.image}
-                className={styles.nftImage}
-                style={{
-                  objectFit: "contain",
-                  borderRadius: "15px",
-                }}
-                alt="nft"
-              />
-            </div>
-          ))}
-          <button
-            className={styles.btnClaim}
-            onClick={() => claim({ amount: 1 })}
-          >
-            Claim NFT
-          </button>
-        </div>
+          <div className={styles.nftCard}>
+            <h2>Claim your NFT!</h2>
+            {nft?.map((nft) => (
+              <div key={nft.metadata.id} className={styles.nftCard}>
+                <MediaRenderer
+                  src={nft.metadata.image}
+                  className={styles.nftImage}
+                  style={{
+                    objectFit: "contain",
+                    borderRadius: "15px",
+                  }}
+                  alt="nft"
+                />
+              </div>
+            ))}
+          </div>
 
+          {wallet.connected ? (
+            <button
+              className={styles.btnClaim}
+              onClick={() => claim({ amount: 1 })}
+            >
+              Claim NFT
+            </button>
+          ) : (
+            <button
+              className={styles.btnClaim}
+              onClick={() => claim({ amount: 1 })}
+            >
+              Connect your wallet
+            </button>
+          )}
+        </div>
         <footer className={styles.footer}>
           <a
             className={styles.Links}
@@ -83,7 +89,15 @@ const Home = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Powered by NFTLAB
+            Powered by{" "}
+          </a>
+          <a
+            className={styles.Links}
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            NFT LAB CLUB
           </a>
         </footer>
       </div>
